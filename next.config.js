3fe2d61670595bd9752b2fcad105d4a61a58e9fc
unknown <bryanthuamcorporativo@gmail.com>
2025-08-10 23:29:53 -0500

    chore: eliminar next.config.js innecesario

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Permite que el build pase aunque haya errores de ESLint
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
